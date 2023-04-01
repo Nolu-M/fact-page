8cb182c094c066460300dca56bc7846f8c3731c9
Nolu-M <119700411+Nolu-M@users.noreply.github.com>
2023-04-01 22:22:36 +0200

    html and css

---
+++ fact-page/src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
+
+export default function Main() {
+    return (
+        <main background-image="react-icon-large.png" alt="background react image">
+            <h1 className="main-facts">Fun facts about React</h1>
+            <ul className="bullet-points">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Kordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprice apps, including <br/>mobile apps</li>
+            </ul>
+        </main>
+    )
+}
---
+++ fact-page/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
+
+export default function Navbar() {
+    return (
+        <nav className="navbar-container">
+        <img src="/src/images/react-logo.png" className="react"/>
+        <h3 className="nav-facts">ReactFacts</h3>
+        <h4 className="project">React Course - Project 1</h4>
+        </nav>
+    )
+}
