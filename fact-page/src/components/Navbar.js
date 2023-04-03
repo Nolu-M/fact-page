d52f68f06c3885a4619f810a28510b405fa033de
Nolu-M <119700411+Nolu-M@users.noreply.github.com>
2023-04-03 15:26:48 +0200

    final changes

--- fact-page/src/components/Navbar.js
+++ fact-page/src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
-//import ReactDOM from 'react-dom/client';
+import myImage from '../images/react-logo.png';
+
 
 export default function Navbar() {
     return (
         <nav className="navbar-container">
-        <img src="../react-logo.png" className="react"/>
+        <img src={require("../images/react-logo.png")} className="react" alt="react-logo"/>
         <h3 className="nav-facts">ReactFacts</h3>
         <h4 className="project">React Course - Project 1</h4>
         </nav>
